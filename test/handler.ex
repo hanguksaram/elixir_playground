bc45939ffc0f59b788b8074d0a85013a6d61dd27
javaboy <artur.pozhidaev@gmail.com>
2018-10-04 09:50:56 +0700

    conv struct

--- test/handler.ex
+++ test/handler.ex
@@ -1,4 +1,12 @@
 defmodule Test.Handler do
+
+  alias Test.Conv
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  import Test.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Test.Parser, only: [parse: 1]
+
   def handle(request)  do
     request
     |> parse
@@ -9,54 +17,43 @@ defmodule Test.Handler do
     |> format_response
   end
 
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(conv), do: conv
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-
-    %{  method: method,
-        path: path,
-        resp_body: "",
-        status: nil }
-  end
-
-  def log(conv), do: IO.inspect conv
-
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
-
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(%{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  #строка на эльфийском определенно ))
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+
+
+  def route(%Conv{ method: "GET", path: "/about" } = conv) do
+      @pages_path
+      |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
+  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%{ path: path } = conv) do
+  def route(%Conv{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def format_response(conv) do
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
@@ -77,7 +74,7 @@ defmodule Test.Handler do
   end
 end
 request = """
-GET /wildthings HTTP/1.1
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
@@ -88,27 +85,5 @@ response = Test.Handler.handle(request)
 
 IO.puts response
 
-request = """
-GET /bears/1 HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
 
-"""
-
-response = Test.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bigfoot HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Test.Handler.handle(request)
-
-IO.puts response
 

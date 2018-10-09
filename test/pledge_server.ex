faff77864361a5a4c3ba782485e6dfdc79b2e9eb
javaboy <artur.pozhidaev@gmail.com>
2018-10-09 14:19:42 +0700

    genserver abstraction

--- test/pledge_server.ex
+++ test/pledge_server.ex
@@ -1,57 +1,95 @@
+defmodule Test.GenericServer do
+
+  def start(initial_state, name) do
+
+    pid = spawn(__MODULE__, :listen_loop, [initial_state])
+    Process.register(pid, name)
+    pid
+  end
+
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+
+    receive do {:response, response} -> response end
+  end
+
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  def listen_loop(state) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = Test.PledgeServer.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state)
+      {:cast, message }  ->
+        new_state = Test.PledgeServer.handle_cast(message, state)
+        listen_loop(new_state)
+      unexpected ->
+        IO.puts "Unexpected messaged: #{inspect unexpected}"
+        listen_loop(state)
+    end
+  end
+end
+
+
 defmodule Test.PledgeServer do
 
   #client interface
   @name :pledge_server
+
+  alias Test.GenericServer
+
+
   def start do
-    IO.puts "Starting the pledge server..."
-    pid = spawn(__MODULE__, :listen_loop, [[]])
-    Process.register(pid, @name)
-    pid
+    IO.puts "Starting the pledge server"
+    GenericServer.start([], @name)
   end
-
   def create_pledge(name, amount) do
-    send @name, {self(), :create_pledge, name, amount}
-
-    receive do {:response, status} -> status end
+    GenericServer.call @name, {:create_pledge, name, amount}
   end
 
   def recent_pledges() do
-    send @name, {self(), :recent_pledges}
-
-    receive do {:response, pledges} -> pledges end
+    GenericServer.call @name, :recent_pledges
 
   end
 
   def total_pledged do
-    send @name, {self(), :total_pledged}
-
-    receive do {:response, total} -> total end
-
+    GenericServer.call @name, :total_pledged
   end
+
+  def clear do
+    GenericServer.cast @name, :clear
+  end
+
+  #Helper Functions
+
+
 
   # Server
 
-  def listen_loop(state) do
-    IO.puts "\nWaiting for a message..."
 
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(state, 2)
-        new_state = [{name, amount} | most_recent_pledges]
-        send sender, {:response, id}
-        listen_loop(new_state)
-      {sender, :recent_pledges} ->
-        send sender, {:response, state}
-        listen_loop(state)
-      {sender, :total_pledged} ->
-        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-        send sender, {:response, total}
-        listen_loop(state)
 
-    end
+  def handle_cast(:clear, _state) do
+    []
   end
 
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state, 2)
+    new_state = [{name, amount} | most_recent_pledges]
+    {id, new_state}
+
+  end
+
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
+  def handle_call(:total_pledged, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+    {total, state}
+  end
 
 
   defp send_pledge_to_service(_name, _amount) do
@@ -62,11 +100,14 @@ end
 alias Test.PledgeServer
 
 pid = PledgeServer.start()
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("moe", 20)
 
-IO.inspect PledgeServer.recent_pledges()
+
+
+PledgeServer.create_pledge("moe", 20)
+ PledgeServer.create_pledge("moe", 20)
+ PledgeServer.create_pledge("moe", 20)
+ PledgeServer.create_pledge("moe", 20)
+ PledgeServer.create_pledge("moe", 20)
+ PledgeServer.clear()
+ PledgeServer.recent_pledges()
 IO.inspect PledgeServer.total_pledged()

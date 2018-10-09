e6c2feebfed788a47c9b8ee42de427c75a5ca70a
javaboy <artur.pozhidaev@gmail.com>
2018-10-09 13:24:13 +0700

    inproccess server state handler

--- test/pledge_controller.ex
+++ test/pledge_controller.ex
@@ -1,16 +1,16 @@
 defmodule Test.PledgeController do
   def create(conv, %{"name" => name, "amount" => amount}) do
 
-    # Test.PledgeServer.create_pledge(name, String.to_integer(amount))
+    Test.PledgeServer.create_pledge(name, String.to_integer(amount))
 
-    # %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!"}
+    %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!"}
   end
 
   def index(conv) do
 
-    # pledges = Test.PledgeServer.recent_pledges()
+    pledges = Test.PledgeServer.recent_pledges()
 
-    # %{ conv | status: 200, resp_body: (inspect pledges)}
+    %{ conv | status: 200, resp_body: (inspect pledges)}
   end
 
 
--- test/pledge_server.ex
+++ test/pledge_server.ex
@@ -1,9 +1,35 @@
 defmodule Test.PledgeServer do
 
+  #client interface
+  @name :pledge_server
   def start do
     IO.puts "Starting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
   end
+
+  def create_pledge(name, amount) do
+    send @name, {self(), :create_pledge, name, amount}
+
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledges() do
+    send @name, {self(), :recent_pledges}
+
+    receive do {:response, pledges} -> pledges end
+
+  end
+
+  def total_pledged do
+    send @name, {self(), :total_pledged}
+
+    receive do {:response, total} -> total end
+
+  end
+
+  # Server
 
   def listen_loop(state) do
     IO.puts "\nWaiting for a message..."
@@ -18,21 +44,15 @@ defmodule Test.PledgeServer do
       {sender, :recent_pledges} ->
         send sender, {:response, state}
         listen_loop(state)
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+        send sender, {:response, total}
+        listen_loop(state)
+
     end
   end
 
-  def create_pledge(pid, name, amount) do
-    send pid, {self(), :create_pledge, name, amount}
 
-    receive do {:response, status} -> status end
-  end
-
-  def recent_pledges(pid) do
-    send pid, {self(), :recent_pledges}
-
-    receive do {:response, pledges} -> pledges end
-
-  end
 
   defp send_pledge_to_service(_name, _amount) do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
@@ -42,9 +62,11 @@ end
 alias Test.PledgeServer
 
 pid = PledgeServer.start()
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.recent_pledges(pid)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+
+IO.inspect PledgeServer.recent_pledges()
+IO.inspect PledgeServer.total_pledged()

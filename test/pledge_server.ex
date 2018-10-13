7bfad06c5a786f3b96df4710d9d2b85b92ff7e1b
hanguksaram <artur.pozhidaev@gmail.com>
2018-10-13 13:56:09 +0700

    genserver

--- test/pledge_server.ex
+++ test/pledge_server.ex
@@ -1,66 +1,41 @@
-defmodule Test.GenericServer do
-
-  def start(initial_state, name) do
-
-    pid = spawn(__MODULE__, :listen_loop, [initial_state])
-    Process.register(pid, name)
-    pid
-  end
-
-  def call(pid, message) do
-    send pid, {:call, self(), message}
-
-    receive do {:response, response} -> response end
-  end
-
-  def cast(pid, message) do
-    send pid, {:cast, message}
-  end
-
-  def listen_loop(state) do
-    receive do
-      {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = Test.PledgeServer.handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state)
-      {:cast, message }  ->
-        new_state = Test.PledgeServer.handle_cast(message, state)
-        listen_loop(new_state)
-      unexpected ->
-        IO.puts "Unexpected messaged: #{inspect unexpected}"
-        listen_loop(state)
-    end
-  end
-end
-
 
 defmodule Test.PledgeServer do
 
   #client interface
   @name :pledge_server
 
-  alias Test.GenericServer
+  use GenServer
+
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
 
 
   def start do
     IO.puts "Starting the pledge server"
-    GenericServer.start([], @name)
+    GenServer.start(__MODULE__,%State{} , name: @name)
   end
+
   def create_pledge(name, amount) do
-    GenericServer.call @name, {:create_pledge, name, amount}
+    GenServer.call @name, {:create_pledge, name, amount}
   end
 
   def recent_pledges() do
-    GenericServer.call @name, :recent_pledges
+    GenServer.call @name, :recent_pledges
 
   end
 
   def total_pledged do
-    GenericServer.call @name, :total_pledged
+    GenServer.call @name, :total_pledged
   end
 
   def clear do
-    GenericServer.cast @name, :clear
+    GenServer.cast @name, :clear
+  end
+
+  def set_cache_size(size) do
+    GenServer.cast @name, {:set_cache_size, size}
   end
 
   #Helper Functions
@@ -69,45 +44,64 @@ defmodule Test.PledgeServer do
 
   # Server
 
-
-
-  def handle_cast(:clear, _state) do
-    []
+  def init(state) do
+    pledges = fetch_recent_pledges_from_service()
+    new_state = %{state | pledges: pledges}
+    {:ok, new_state}
   end
 
-  def handle_call({:create_pledge, name, amount}, state) do
+  def handle_cast(:clear, state) do
+    {:noreply, %{state | pledges: []}}
+  end
+  def handle_cast({:set_cache_size, size}, state) do
+    new_state = %{ state | cache_size: size}
+    {:noreply, new_state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} = send_pledge_to_service(name, amount)
-    most_recent_pledges = Enum.take(state, 2)
-    new_state = [{name, amount} | most_recent_pledges]
-    {id, new_state}
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
+    cached_pledges = [{name, amount} | most_recent_pledges]
+    new_state = %{state | pledges: cached_pledges }
+    {:reply, id, new_state}
 
   end
 
-  def handle_call(:recent_pledges, state) do
-    {state, state}
+  def handle_call(:recent_pledges, _from, state) do
+    {:reply, state.pledges, state}
   end
-  def handle_call(:total_pledged, state) do
-    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-    {total, state}
+  def handle_call(:total_pledged, _from, state) do
+    total = Enum.map(state.pledges, &elem(&1, 1)) |> Enum.sum
+    {:reply, total, state}
+  end
+
+  def handle_info(_message, state) do
+    IO.puts "Cant touch this"
+    {:noreply, state}
   end
 
 
   defp send_pledge_to_service(_name, _amount) do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
+  defp fetch_recent_pledges_from_service do
+    [{"wilma", 15}, {"fred", 25}]
+  end
 end
 
 alias Test.PledgeServer
 
-pid = PledgeServer.start()
+{:ok, pid} = PledgeServer.start()
 
-
-
+PledgeServer.set_cache_size(4)
 PledgeServer.create_pledge("moe", 20)
- PledgeServer.create_pledge("moe", 20)
- PledgeServer.create_pledge("moe", 20)
- PledgeServer.create_pledge("moe", 20)
- PledgeServer.create_pledge("moe", 20)
- PledgeServer.clear()
- PledgeServer.recent_pledges()
+# PledgeServer.clear()
+
+
+#  PledgeServer.create_pledge("moe", 20)
+#  PledgeServer.create_pledge("moe", 20)
+#  PledgeServer.create_pledge("moe", 20)
+#  PledgeServer.create_pledge("moe", 20)
+
+ IO.inspect PledgeServer.recent_pledges()
 IO.inspect PledgeServer.total_pledged()
